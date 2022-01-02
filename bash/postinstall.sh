4d9784e48e8af30a6488819f159ee019675e3f56
Mogria <m0gr14@gmail.com>
2022-01-02 03:28:23 +0100

    bash: improve ble.sh install script

--- bash/postinstall.sh
+++ bash/postinstall.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
-set -eu
-install_dir=~/.local/share/ble.sh
+set -eux
+install_dir="$HOME/.local/share/ble.sh"
+
+
+if [ -f "$install_dir/ble.sh" ]; then
+  echo "Skipping build of ble.sh, seems already installed in $install_dir"
+  exit 0
+fi
 
 ( cd "$TMPDIR";
-  test ! -d "ble.sh" && git clone --recursive https://github.com/akinomyoga/ble.sh.git
+  test -d "ble.sh" && exit 0
+  git clone --recursive https://github.com/akinomyoga/ble.sh.git
   cd ble.sh
+  git checkout v0.3.3
   make install INSDIR="$install_dir"
 )
 
